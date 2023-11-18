f7595086142a3e397c74a0b07d3a997da7365e49
Quan Ngo <ngotrungquan1412@gmail.com>
2023-11-18 14:13:15 +0700

    SideBar+Header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,67 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import { FaBell } from 'react-icons/fa6'
+import { FaAngleLeft } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+Header.propTypes = {
+    info: PropTypes.element.isRequired
+};
 const cx = classNames.bind(styles);
 
-function Header() {
+function Header(info) {
+    let navigate = useNavigate();
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}>Header</div>
+            <Container className={cx('header')}>
+                <Row className='w-100 h-100'>
+                    <Col xs md={8}>
+                        {
+                            info.info.title === 'Quay lại' ? (
+                                <a className={`${cx('title-return')} text-start d-flex align-items-center`} href='#' onClick={() => navigate(-1)}>
+                                    <FaAngleLeft className='me-2' />
+                                    Quay lại
+                                </a>
+                            ) : (
+                                <p className={`${cx('title')} text-start d-flex align-items-center`}>
+                                    {info.info.title}
+                                </p>
+                            )
+                        }
+
+                    </Col>
+                    <Col xs md={4} className='align-items-center'>
+                        <Row >
+                            <Col xs md={4} className='text-end d-flex justify-content-end align-items-center'>
+                                <FaBell className={cx('header-icon')} />
+                            </Col>
+                            <Col xs md={8} >
+                                <Row className='text-end'>
+                                    <Col xs md={3}>
+                                        <Image src={info.info.img} roundedCircle fluid className={`${cx('avatar')} p-1`} />
+                                    </Col >
+                                    <Col xs md={9} className='text-start d-flex align-items-center'>
+                                        {info.info.name}
+                                    </Col>
+
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+
+
+
+
         </div>
     );
 }
--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,24 +1,302 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FaHouse } from 'react-icons/fa6'
+import { FaBuffer } from 'react-icons/fa6'
+import { FaWallet } from 'react-icons/fa6'
+import { FaChartSimple } from 'react-icons/fa6'
+import { FaBusinessTime } from 'react-icons/fa6'
+import { FaCartShopping } from 'react-icons/fa6'
+import { FaUser } from 'react-icons/fa6'
+import { FaUsers } from 'react-icons/fa6'
+import Accordion from 'react-bootstrap/Accordion';
+import logo from '../../../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const cx = classNames.bind(styles);
 
 function SideBar() {
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
-                <div className={cx('sth')}></div>
+            <div className={cx('Menubar inner')}>
+
+                <div className='d-flex align-items-center '>
+                    <img src={logo} alt="" className={cx('my-icon')}></img>
+                    <p className={cx('name')}> Triple K</p>
+                </div>
+
+
+
+
+                <p class="text-wrap ms-2" href='#'>
+                    <NavLink to="/overview" className={(navData) => navData.isActive ?
+                        'btn bg-primary text-white w-100 text-start' :
+                        'btn w-100 text-start'} >
+                        <Row>
+                            <Col xs="2" md="2">
+                                <FaHouse />
+                            </Col>
+
+                            <Col xs="10" md="10" >
+                                Tổng quan
+                            </Col>
+                        </Row>
+
+                    </NavLink>
+
+
+
+                </p>
+
+                <div>
+                    <Accordion alwaysOpen>
+
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header ><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaCartShopping />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Đơn hàng
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/2" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+
+                                <NavLink to="/2" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+
+                        <Accordion.Item eventKey="1" >
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaBuffer />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Sản phẩm
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100' >
+                                <NavLink to="/ListProduct" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`} >
+                                    <li className={cx('my-li')}>Danh sách sản phẩm</li>
+
+                                </NavLink>
+                                <NavLink to="/3" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Quản lý kho</li>
+                                </NavLink>
+                                <NavLink to="/4" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+
+                                </NavLink>
+                                <NavLink to="/5" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Kiểm hàng</li>
+
+                                </NavLink>
+                                <NavLink to="/6" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhà cung cấp</li>
+
+                                </NavLink>
+
+
+
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaUser />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Khách hàng
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/7" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/8" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaWallet />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Sổ quỹ
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/9" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/10" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaChartSimple />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Báo cáo
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/11" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/12" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaBuffer />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Quản lý khuyến mãi
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/13" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/14" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="6">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2">
+                                    < FaUsers />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Quản lý nhân viên
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/15" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/16" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="7">
+                            <Accordion.Header><Row className='w-100'>
+                                <Col xs="2" md="2" className='pe-0'>
+                                    < FaBusinessTime />
+                                </Col>
+
+                                <Col xs="10" md="10" className={cx('accor-header')}>
+                                    Nhật ký hoạt động
+                                </Col>
+
+
+                            </Row>
+                            </Accordion.Header>
+                            <Accordion.Body className='p-0 w-100'>
+                                <NavLink to="/17" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                                <NavLink to="/18" className={(navData) => navData.isActive ?
+                                    `btn bg-primary text-white ${cx('my-navlink')}` :
+                                    `btn ${cx('my-navlink')}`}>
+                                    <li className={cx('my-li')}>Nhập hàng</li>
+                                </NavLink>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+
+                </div>
+
             </div>
         </div>
     );

b4185c925d2870165b0ce5c0e967e11549741903
kit <kietphamkb2@gmail.com>
2023-12-10 23:35:44 +0700

    toast notify

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,26 +1,121 @@
+import { useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import styles from './Input.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Input({ title, error, value, onChange, required, ...passProps }) {
+const addCommas = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const removeNonNumeric = (num) => num.toString().replace(/[^0-9]/g, '');
+
+function Input({
+    title,
+    error,
+    value,
+    onChange,
+    onChangeMoney,
+    money,
+    number,
+    onChangeNumber,
+    required,
+    textarea,
+    className,
+    rows,
+    items,
+    ...passProps
+}) {
     const props = {
         ...passProps,
     };
 
+    let Comp = 'input';
+
+    if (textarea) {
+        Comp = 'textarea';
+        props.rows = rows;
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+    });
+
+    // DROPDOWN
+    const [visible, setVisible] = useState(false);
+    const show = () => setVisible(true);
+    const hide = () => setVisible(false);
+
+    const onClickAction = (item) => {
+        onChange(item);
+        hide();
+    };
+
     return (
-        <div className={cx('wapper')}>
-            <div className={cx('title')}>
-                {title}
-                {required && <span className={cx('required')}> *</span>}
-            </div>
-            <input
-                className={cx({ input: true, 'error-border': error })}
-                value={value}
-                onChange={onChange}
-                {...props}
-            />
-            {error && <div className={cx('error')}>{error}</div>}
+        <div className={classes}>
+            <Tippy
+                visible={visible}
+                interactive={true}
+                onClickOutside={hide}
+                placement="bottom"
+                zIndex={1}
+                render={(attrs) => (
+                    <div
+                        className={cx('item-wrapper')}
+                        tabIndex="-1"
+                        {...attrs}
+                    >
+                        <div className={cx('popper')}>
+                            <ul className={cx('list-item')}>
+                                {items?.map((item, index) => (
+                                    <li
+                                        key={index}
+                                        className={cx('item')}
+                                        onClick={() => onClickAction(item)}
+                                    >
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                )}
+            >
+                <div>
+                    <div className={cx('title')}>
+                        {title}
+                        {required && <span className={cx('required')}> *</span>}
+                    </div>
+                    <Comp
+                        className={cx({
+                            input: true,
+                            'error-border': error,
+                            'm-h': rows,
+                            't-a': money,
+                        })}
+                        value={value}
+                        onChange={(e) => {
+                            if (money) {
+                                onChangeMoney(
+                                    addCommas(removeNonNumeric(e.target.value)),
+                                );
+                            } else if (number) {
+                                onChangeNumber(
+                                    removeNonNumeric(e.target.value),
+                                );
+                            } else {
+                                onChange(e.target.value);
+                            }
+                        }}
+                        onBlur={() => {
+                            if (money && value === '') {
+                                onChangeMoney(0);
+                            }
+                        }}
+                        onFocus={items && (visible ? hide : show)}
+                        {...props}
+                    />
+                    {error && <div className={cx('error')}>{error}</div>}
+                </div>
+            </Tippy>
         </div>
     );
 }
---
+++ src/components/ToastContext/index.js
@@ -0,0 +1,85 @@
+import { createContext } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ToastContext = createContext();
+
+function ToastProvider({ children }) {
+    const notify = (type, message) => {
+        // eslint-disable-next-line default-case
+        switch (type) {
+            case 'info':
+                toast.info(message, {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+                break;
+            case 'success':
+                toast.success(message, {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+                break;
+            case 'warning':
+                toast.warn(message, {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+                break;
+            case 'error':
+                toast.error(message, {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+                break;
+        }
+    };
+
+    const value = {
+        notify,
+    };
+
+    return (
+        <ToastContext.Provider value={value}>
+            {children}
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </ToastContext.Provider>
+    );
+}
+
+export { ToastContext, ToastProvider };

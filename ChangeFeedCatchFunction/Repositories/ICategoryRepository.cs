0a56b15efa332bfbd9727c80d01cd2b1c6074eb3
TrungKien <trungkien2003ntk@gmail.com>
2023-12-19 05:05:54 +0700

    Create Azure Function To handle Change Feed On Category Container Cosmos Db

---
+++ ChangeFeedCatchFunction/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChangeFeedCatchFunction.Models;
+
+namespace ChangeFeedCatchFunction.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task DeleteCategoryAsync(Category updatedCategory);
+        Task<Category> GetCategoryDocumentByIdAsync(string categoryId);
+    }
+}
